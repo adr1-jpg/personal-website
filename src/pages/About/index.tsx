2a8198b49761582fbfcb49f982df7dd57fb85d53
adr1-jpg <orantesadri1@gmail.com>
2024-12-01 19:27:48 -0800

    made about responsive

--- src/pages/About/index.tsx
+++ src/pages/About/index.tsx
@@ -3,14 +3,16 @@ import nyc from '../../assets/images/nyc-me.jpeg'
 import cs from '../../assets/images/me-cs.jpg'
 import Header from '../../components/Header'
 import Footer from '../../components/Footer'
+import './about.css'
 const About: React.FC = () => {
     return (
         <div className="bg-background">
             <Header />
-            <div className="justify-items-center bg-background">
-                <div className="flex max-w-[1024px] w-full py-20 content-center gap-16">
-                    <div className="flex flex-col gap-6 leading-normal basis-1/2">
-                        <p className="font-lora text-[50px] ">Nice to meet you</p>
+            
+            <div className="justify-items-center bp">
+                <div className="flex about-section max-w-[1024px] w-full py-20 content-center gap-16">
+                    <div className="flex flex-col about-desc leading-normal basis-1/2">
+                        <p className="font-lora text-[50px] about-header">Nice to meet you</p>
                         <p className="font-openSans leading-normal">I'm a Salvadoran American passionate about being playful, curious, and bringing people together.<br /><br />
                             Growing up, I had no clear picture of the future I was working toward. I just knew it would be something that would inspire and challenge me, while also bringing a sense of stability, fulfillment and joy.<br /><br />
                             I first discovered computer science in high school through a summer program at MIT. It was tough—I had never experienced such academic rigor. But by proactively seeking support, I persevered.</p>
@@ -21,14 +23,14 @@ const About: React.FC = () => {
                     </div>
                 </div>
             </div>
-            <div className="justify-items-center bg-background">
+            <div className="justify-items-center bp">
                 
-                <div className="flex max-w-[1024px] w-full py-10 content-center gap-10 rounded-[20px]">
+                <div className="flex about-section max-w-[1024px] w-full py-10 content-center gap-10 rounded-[20px]">
                 <div className="basis-1/2">
                         <img src={cs} className="rounded-[20px]"/>
                     </div>
                     <div className="flex flex-col gap-6 leading-normal basis-1/2">
-                        <p className="font-lora text-[50px] ">I can, and I will</p>
+                        <p className="font-lora text-[50px] about-header">I can, and I will</p>
                         <p className="font-openSans leading-normal">
                             For undergrad, I attended Columbia University's School of Engineering. I majored in Computer Science with a focus on applications.
 
@@ -38,8 +40,6 @@ const About: React.FC = () => {
                     
                 </div>
             </div>
-
-
             <Footer />
         </div>
     );
